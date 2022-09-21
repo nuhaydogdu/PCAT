600637a7a23d09f98dd432c3a24db14edd6df3a9
Nuh Aydoğdu <nuhaydd25@gmail.com>
2022-09-21 12:42:29 +0300

    model and dynamic content

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require('express'); //schema (skima: şablon)
-const app = express();
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const path = require('path');
 //path için npm init yapmadan kullanabiliyoruz
-const ejs = require('ejs');
+const Photo = require('./models/Photo');
+
+const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPPLATE ENGİNE
 app.set('view engine', 'ejs');
 
 //MİDDLEWARE
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTERS
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   /* res.sendFile(path.resolve(__dirname, '.temp/index.html'));
   //resolve dosya yolunu çözümlemesi için
   //__dirname : proje klasörümün olduğu yer
   //public dosyasını static yaparak bunu sağladık
  */
-  res.render('index');
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -31,10 +42,17 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.get('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
 
 app.listen(3000, () => {
   console.log('server 3000 portundan ayaklandı');
